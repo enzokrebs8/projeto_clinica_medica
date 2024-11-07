d4a0e3c456ff73be664074c2d85fa3c50728c6d6
enzokrebs8 <enzokrebs8@gmail.com>
2024-11-07 11:43:21 -0300

    Aiaiaiaiaiaiaiaiaiaiaiaiai

--- conecta.php
+++ conecta.php
@@ -6,6 +6,16 @@
 
     $conexao = new mysqli($servidor,$usuario,$senha,$banco);
 
+    $sql= "SET NAMES utf8'";
+    mysqli_query($conexao, $sql);
+    $sql= 'SET NAMES character_set_connection=utf8';
+    mysqli_query($conexao, $sql); 
+    $sql= 'SET NAMES character_set_client=utf8';
+    mysqli_query($conexao, $sql); 
+    $sql= 'SET character_set_results=utf8';
+    mysqli_query($conexao, $sql); 
+
+
     if(mysqli_connect_errno()){
         echo "Erro na conexão com o Banco de Dados!";
     }   
