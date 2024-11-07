5c83c6d6af2a08b8ec8c2d6a81d81feb8e67a460
enzokrebs8 <enzokrebs8@gmail.com>
2024-11-07 09:06:07 -0300

    aiaiai

--- processa_cadastro.php
+++ processa_cadastro.php
@@ -2,34 +2,50 @@
 
     require('conecta.php');
 
-    $nome = $_POST['nome'];
-    $genero = $_POST['genero'];
-    $nascimento = $_POST['nascimento'];
-    $RG = $_POST['RG'];
-    $CPF = $_POST['CPF'];
-    $email = $_POST['email'];
-    $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
-    $telefone = $_POST['telefone'];
-    $telefoneEmergencia = $_POST['telefoneEmergencia'];
-    $IDEndereco = $_POST['IDEndereco'];
-    $IDPlanoSaude = $_POST['IDPlanoSaude'];
+    // $nome = $_POST['nome'];
+    // $genero = $_POST['genero'];
+    // $nascimento = $_POST['nascimento'];
+    // $RG = $_POST['RG'];
+    // $CPF = $_POST['CPF'];
+    // $email = $_POST['email'];
+    // $senha = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+    // $telefone = $_POST['telefone'];
+    // $telefoneEmergencia = $_POST['telefoneEmergencia'];
+    // $IDEndereco = $_POST['IDEndereco'];
+    // $IDPlanoSaude = $_POST['IDPlanoSaude'];
+
+    $nome = 'Enzo Krebs Silva';
+    $genero = 'Masculino';
+    $nascimento = '2000-11-24';
+    $RG = '123456789101';
+    $CPF = '464867821';
+    $email = 'enzok8@email.com';
+    $senha = password_hash('123456', PASSWORD_DEFAULT);
+    $telefone = '11 40028922';
+    $telefoneEmergencia = '11 40028922';
+    $IDEndereco = '1';
+    $IDPlanoSaude = '1';
 
     $data_atual = new DateTime();
     $data_nascimento_formatada = new DateTime($nascimento);
     $idade = $data_atual->diff($data_nascimento_formatada)->y;
 
     if ($idade >= 18) {
-        $IDPacienteMaior = $_POST['IDPacienteMaior'];
+        $IDPacienteMaior = '1';
         $add = "INSERT INTO pacientemaior (genero, nascimento, RG, nome, email, senha, telefoneEmergencia, telefone, CPF, IDEndereco, IDPlanoSaude) VALUES ('$genero','$nascimento','$RG','$nome','$email','$senha','$telefoneEmergencia','$telefone','$CPF','$IDEndereco','$IDPlanoSaude')";
+        echo "Qualquer coisa";
         $conexao->query($add);
         header('Location: login.html');
     } else {
         $IDPacienteMenor = $_POST['IDPacienteMenor'];
         $relacaoResponsavel = $_POST['relacaoResponsavel'];
         $IDResponsavel = $_POST['IDResponsavel'];
+        echo "Qualquer coisa";
+        return 0;
         $add = "INSERT INTO pacientemenor (IDPacienteMenor, relacaoResponsavel, telefone, telefoneEmergencia, nascimento, email, senha, nome, RG, CPF, genero, IDResponsavel, IDEndereco, IDPlanoSaude) VALUES ('$IDPacienteMenor','$relacaoResponsavel','$telefone','$telefoneEmergencia','$nascimento','$email','$senha','$nome','$RG','$CPF','$genero','$IDResponsavel','$IDEndereco','$IDPlanoSaude')";
         $conexao->query($add);
         header('Location: login.html');
+        
     }
 
 ?>
