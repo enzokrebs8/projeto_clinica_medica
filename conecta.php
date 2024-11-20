3c493861af96978d2f9c09617f251812f12b6fd5
enzokrebs8 <enzokrebs8@gmail.com>
2024-11-20 19:46:18 -0300

    Trabalho duro

--- conecta.php
+++ conecta.php
@@ -1,22 +1,16 @@
 <?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "";
-    $banco = "clinicamedica";
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "clinicamedica";
 
-    $conexao = new mysqli($servidor,$usuario,$senha,$banco);
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
-    $sql= "SET NAMES utf8";
-    mysqli_query($conexao, $sql);
-    $sql= "SET NAMES character_set_connection=utf8";
-    mysqli_query($conexao, $sql);
-    $sql= "SET NAMES character_set_client=utf8";
-    mysqli_query($conexao, $sql); 
-    $sql= "SET character_set_results=utf8";
-    mysqli_query($conexao, $sql); 
+if ($conexao->connect_errno) {
+    echo "Erro na conexão com o Banco de Dados!";
+    exit();
+}
 
-
-    if(mysqli_connect_errno()){
-        echo "Erro na conexão com o Banco de Dados!";
-    }   
+$sql = "SET NAMES utf8";
+mysqli_query($conexao, $sql);
 ?>
