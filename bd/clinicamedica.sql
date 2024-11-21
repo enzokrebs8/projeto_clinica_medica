fcd0fd849dd9e2e4ea690857659d573c568915a0
enzokrebs8 <enzokrebs8@gmail.com>
2024-11-21 00:57:25 -0300

    Trabalho duro

--- bd/clinicamedica.sql
+++ bd/clinicamedica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 20-Nov-2024 às 23:28
+-- Tempo de geração: 21-Nov-2024 às 03:57
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -64,10 +64,10 @@ CREATE TABLE IF NOT EXISTS `consultaspaciente` (
 DROP TABLE IF EXISTS `devs`;
 CREATE TABLE IF NOT EXISTS `devs` (
   `nome` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `cpf` varchar(14) COLLATE utf8mb4_general_ci NOT NULL,
+  `CPF` varchar(14) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `senha` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`cpf`),
+  PRIMARY KEY (`CPF`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -134,7 +134,14 @@ CREATE TABLE IF NOT EXISTS `medicos` (
   `CRM` varchar(9) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `telefone` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`IDMedico`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `medicos`
+--
+
+INSERT INTO `medicos` (`IDMedico`, `CPF`, `email`, `senha`, `RG`, `nome`, `Especialidade`, `nascimento`, `CRM`, `telefone`) VALUES
+(4, '464.048.678-26', 'medicoenzo@email.com', '$2y$10$6wQOkqaSyJ4v1gTMrNzSmu4UkGYOWlbA.bCnzgW22K6qCyvMiYgDO', '123456789101', 'Enzo Krebs Silva', 'Neurologista', '2000-11-24', '123123123', '(11) 93357-2695');
 
 -- --------------------------------------------------------
 
