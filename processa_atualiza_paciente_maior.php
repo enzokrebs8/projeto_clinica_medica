637e7b75c2da257ed1a24b174293a279e4583ad9
enzokrebs8 <enzokrebs8@gmail.com>
2024-11-21 03:05:10 -0300

    Trabalho duro

---
+++ processa_atualiza_paciente_maior.php
@@ -0,0 +1,22 @@
+<?php
+    require('conecta.php');
+
+    $nome_novo = $_POST['nome'];
+    $CPF_novo = $_POST['CPF'];
+    $RG_novo = $_POST['RG'];
+    $nascimento_novo = $_POST['nascimento'];
+    $telefone_novo = $_POST['telefone'];
+    $email_novo = $_POST['email'];
+    $telefoneEmergencia_novo = $_POST['telefoneEmergencia'];
+    $genero = $_POST['genero'];
+    $senha = $_POST['senha'];
+    $hash = password_hash($senha, PASSWORD_BCRYPT);
+    $IDEndereco_novo = $_POST['IDEndereco'];
+    
+    $consulta = "UPDATE pacientemaior SET nome = '$nome_novo', email = '$email_novo', senha = '$hash', telefone='$telefone_novo', telefoneEmergencia='$telefoneEmergencia_novo', RG='$RG_novo', CPF='$CPF_novo', nascimento='$nascimento_novo', IDEndereco='$IDEndereco_novo' WHERE IDPacienteMaior = '$id'";
+
+    $conexao->query($consulta);
+
+    header('Location: index.php')
+
+?>
