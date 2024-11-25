05364129b11baa5e94f2cd8775cff03c622d09bc
enzokrebs8 <enzokrebs8@gmail.com>
2024-11-25 03:26:08 -0300

    Trabalho duro

--- cadastro_medico.php
+++ cadastro_medico.php
@@ -74,7 +74,7 @@ if (!isset($_SESSION['tipo']) || $_SESSION['tipo'] != 'devs') {
                                             <label for="txtEmail">Endereço de Email</label>
                                         </div>
                                         <div class="row mb-3">
-                                            <div class="col-md-6">
+                                            <div>
                                                 <div class="form-floating mb-3 mb-md-0">
                                                     <input class="form-control" id='txtSenha' name="senha" type="password" placeholder="" required/>
                                                     <label for="txtSenha">Senha</label>
